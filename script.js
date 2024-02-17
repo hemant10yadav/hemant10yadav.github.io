0006609380fffeab2b22eb1eefb50e79bb214b30
Hemant Singh Yadav <98798073+hemant10yadav@users.noreply.github.com>
2024-02-17 11:24:39 +0530

    Added a video for work section

--- script.js
+++ script.js
@@ -114,3 +114,27 @@ const saveData = async () => {
     }
 
 }
+
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+};
+
+const callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+        } else {
+            entry.target.classList.remove('in-view');
+        }
+    });
+};
+
+const observer = new IntersectionObserver(callback, options);
+const allCards = document.querySelectorAll('.card');
+allCards.forEach(card => {
+    observer.observe(card);
+});
+
