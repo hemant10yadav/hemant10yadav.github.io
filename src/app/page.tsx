02bdaa685ca6e31951a2a5858d4755317faa566f
hemant10yadav <hemant.10.yadav@gmail.com>
2025-01-05 11:58:32 +0530

    resolved merge conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,16 @@ import { ProjectSection } from "./components/ProjectSection";
 import ExperienceSection from "./components/ExperienceSection";
 import { NavBar } from "./components/Navbar";
 import { HeroSection } from "./components/HeroSection";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function Home() {
-
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-950 text-white">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900/20 via-transparent to-transparent">
-         <NavBar/>
+        <GoogleAnalytics trackPageViews />
+        <NavBar />
         <main className="pt-20">
-          <HeroSection/>
+          <HeroSection />
           <ExperienceSection />
           <section id="skills">
             <SkillSection />
