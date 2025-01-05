83a218300de4396b6c2ca3f8acd579e5ffd9c469
hemant10yadav <hemant.10.yadav@gmail.com>
2025-01-05 16:00:44 +0530

    added breakpoint for smaller screen

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,21 +1,24 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export const NavBar = () => {
   const [activeSection, setActiveSection] = useState("about");
 
   return (
     <nav className="fixed top-0 w-full bg-black/50 backdrop-blur-lg z-50">
-      <div className="mx-2 px-16 py-4">
+      <div className="mx-2 px-2 sm:px-16 py-4">
         <div className="flex justify-between items-center">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            className="text-xl font-bold bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent"
-          >
-            <Image alt="logo" src="/assets/hsy.png" width={80} height={160} />
-          </motion.div>
+          <Link href="/" passHref>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              className="text-xl font-bold bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent cursor-pointer"
+            >
+              <Image alt="logo" src="/assets/hsy.png" width={80} height={160} />
+            </motion.div>
+          </Link>
           <div className="flex gap-8 text-lg">
             {["about", "skills", "projects"].map((item) => (
               <motion.a
