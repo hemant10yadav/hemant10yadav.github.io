2301173b19005cc0fcfb682437af45d75b733234
hemant10yadav <hemant.10.yadav@gmail.com>
2025-01-05 15:51:03 +0530

    optimized images

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,21 +1,22 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import Image from "next/image";
 
 export const NavBar = () => {
   const [activeSection, setActiveSection] = useState("about");
-  
+
   return (
     <nav className="fixed top-0 w-full bg-black/50 backdrop-blur-lg z-50">
-      <div className="container mx-auto px-6 py-4">
+      <div className="mx-2 px-16 py-4">
         <div className="flex justify-between items-center">
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             className="text-xl font-bold bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent"
           >
-            HSY
+            <Image alt="logo" src="/assets/hsy.png" width={80} height={160} />
           </motion.div>
-          <div className="flex gap-8">
+          <div className="flex gap-8 text-lg">
             {["about", "skills", "projects"].map((item) => (
               <motion.a
                 key={item}
