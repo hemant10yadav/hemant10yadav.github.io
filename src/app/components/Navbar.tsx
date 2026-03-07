e45bf1201188c4ba69a9be1d36a736453fd84902
Hemant Singh Yadav <hemant.10.yadav@gmail.com>
2026-03-07 20:08:53 +0530

    removed import

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import { motion } from 'framer-motion';
 import { useState, useEffect } from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 import { useViewer } from '../context/ViewerContext';
 
