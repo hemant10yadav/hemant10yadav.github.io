05a420a6e8a5c563af4d4ecadaa00c7f774c196a
Hemant Singh Yadav <hemant.10.yadav@gmail.com>
2026-09-06 10:33:27 +0530

    build fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from 'next';
 import { SITE_URL } from './constants';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
